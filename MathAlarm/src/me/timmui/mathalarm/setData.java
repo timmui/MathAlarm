41243c8b11e62186c910ab5b0735261f897ee818
tonysuu <tonysu1122@hotmail.com>
2015-01-24 18:52:16 -0500

    broken time picker

---
+++ MathAlarm/src/me/timmui/mathalarm/setData.java
@@ -0,0 +1,19 @@
+package me.timmui.mathalarm;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class setData extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setdata);
+		//name.setText(getIntent().getExtras().getString("userName"));
+		//TimePicker tm = (TimePicker)findViewById(R.id.timePicker1);
+	}
+}
