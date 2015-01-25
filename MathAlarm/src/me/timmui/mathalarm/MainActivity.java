277ebd7d34160523b02bbc1268b69d2979b54d9d
tonysuu <tonysu1122@hotmail.com>
2015-01-24 22:42:48 -0500

    Something?

--- MathAlarm/src/me/timmui/mathalarm/MainActivity.java
+++ MathAlarm/src/me/timmui/mathalarm/MainActivity.java
@@ -3,6 +3,7 @@ package me.timmui.mathalarm;
 import java.util.Calendar;
 
 import android.support.v7.app.ActionBarActivity;
+import android.text.format.DateFormat;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,29 +13,58 @@ import android.widget.*;
 
 public class MainActivity extends ActionBarActivity {
 
-	@Override
+	public String hour;
+	public String minute;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Thread myThread = null;
+		Runnable myRunnableThread = new CountUpRunner();
+		myThread = new Thread(myRunnableThread);
+		myThread.start();
+        
+		
+		Calendar c = Calendar.getInstance();
+        int mHour = c.get(Calendar.HOUR_OF_DAY);
+        int mMinute = c.get(Calendar.MINUTE);
+        int second = c.get(Calendar.SECOND);
 		TextView tv =(TextView)findViewById(R.id.tv1);
-		tv.setText("Pick a time");
+		//tv.setText(mHour+" "+mMinute+" "+second);
+
+		
 		
 		//-------------- Time Picker -----------------
 		final TextView txtTime = (TextView) findViewById(R.id.txtTime);
 		
 		// Process to get Current Time
-        final Calendar c = Calendar.getInstance();
-        int mHour = c.get(Calendar.HOUR_OF_DAY);
-        int mMinute = c.get(Calendar.MINUTE);
 		
 		final TimePickerDialog tm = new TimePickerDialog(this,
         new TimePickerDialog.OnTimeSetListener() {
  
             @Override
-            public void onTimeSet(TimePicker view, int hourOfDay,
-                    int minute) {
-                txtTime.setText("Selected time: "+hourOfDay + ":" + minute);
+            public void onTimeSet(TimePicker view, int hourIn,
+                    int minuteIn) {
+            	hour = Integer.toString(hourIn);
+            	minute = Integer.toString(minuteIn);
+            	if (minuteIn < 10)
+            	{
+            		txtTime.setText("Selected time: "+hourIn + ": 0" + minuteIn);	
+            	}
+            	else
+                txtTime.setText("Selected time: "+hourIn + ":" + minuteIn);
+            	TextView tv1 = (TextView)findViewById(R.id.pickedTime);
+        		//tv1.setText(hour+"  "+minute);
+        		//CharSequence cq = (DateFormat.format("dd/MM/yyyy kk:mm", System.currentTimeMillis()));
+        		//String systemTime = convertTime(cq);
+        		//tv1.setText("current time is "+systemTime);
             }
+
+			/*private String convertTime(CharSequence cq) {
+				// TODO Auto-generated method stub
+				final StringBuilder sb = new StringBuilder(cq.length());
+    			sb.append(cq);
+    			return sb.toString();
+			}*/
         }, mHour, mMinute, false);
 		
 		Button b2 = (Button)findViewById(R.id.b2);
@@ -45,6 +75,20 @@ public class MainActivity extends ActionBarActivity {
 				tm.show ();
 			}
 		});
+	}
+	public void doWork() {
+	    runOnUiThread(new Runnable() {
+	        public void run() {
+	            try{
+	            	Calendar c = Calendar.getInstance();
+	                int mHour = c.get(Calendar.HOUR_OF_DAY);
+	                int mMinute = c.get(Calendar.MINUTE);
+	                int second = c.get(Calendar.SECOND);
+	                TextView tv = (TextView)findViewById(R.id.tv1);
+	                tv.setText(mHour+" "+mMinute+" "+second);
+	            }catch (Exception e) {}
+	        }
+	    });
 	}
 
 	@Override
@@ -65,4 +109,19 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+class CountUpRunner implements Runnable{
+    // @Override
+    public void run() {
+            while(!Thread.currentThread().isInterrupted()){
+                try {
+                doWork();
+                    Thread.sleep(1000); // Pause of 1 Second
+                } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                }catch(Exception e){
+                }
+            }
+    }
+}
 }
