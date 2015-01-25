a511a0d0c43b196dd9719f9531128a7617f9a683
tonysuu <tonysu1122@hotmail.com>
2015-01-24 19:19:47 -0500

    back to main activity

--- MathAlarm/src/me/timmui/mathalarm/setData.java
+++ MathAlarm/src/me/timmui/mathalarm/setData.java
@@ -1,9 +1,12 @@
 package me.timmui.mathalarm;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -15,5 +18,15 @@ public class setData extends ActionBarActivity {
 		setContentView(R.layout.activity_setdata);
 		//name.setText(getIntent().getExtras().getString("userName"));
 		TimePicker tm = (TimePicker)findViewById(R.id.timePicker1);
+		Button back = (Button)findViewById(R.id.back);
+		back.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(setData.this,MainActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
