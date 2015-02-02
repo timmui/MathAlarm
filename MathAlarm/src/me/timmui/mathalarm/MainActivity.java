e51cc91ed01df1f6f17f4fd50b95e0906e472352
tonysuu <tonysu1122@hotmail.com>
2015-02-02 16:50:08 -0500

    fixed delay

--- MathAlarm/src/me/timmui/mathalarm/MainActivity.java
+++ MathAlarm/src/me/timmui/mathalarm/MainActivity.java
@@ -3,84 +3,58 @@ package me.timmui.mathalarm;
 import java.util.Calendar;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends ActionBarActivity {
-	Calendar alarm;
-	Boolean alarmActiviated = false;
+	PendingIntent pi;
+    BroadcastReceiver br;
+    AlarmManager am;
+    final static private long DISPLAY_DELAY = 5000;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		Thread myThread = null;
 
-	    Runnable myRunnableThread = new CountDownRunner();
-	    myThread= new Thread(myRunnableThread);   
-	    myThread.start();
-		
-		//-------------- Time Picker -----------------
-		final TextView txtTime = (TextView) findViewById(R.id.txtTime);
-		
-		// Process to get Current Time
-        final Calendar c = Calendar.getInstance();
-        alarm = Calendar.getInstance();
-        alarm.set(Calendar.YEAR, 3000);
-        alarm.set(Calendar.SECOND, 0);
-        int mHour = c.get(Calendar.HOUR_OF_DAY);
-        int mMinute = c.get(Calendar.MINUTE);
-		
-		final TimePickerDialog tm = new TimePickerDialog(this,
-        new TimePickerDialog.OnTimeSetListener() {
- 
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay,
-                    int minute) {
-                txtTime.setText(String.format("Selected time: %02d:%02d",hourOfDay,minute));
-                
-                alarm.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                alarm.set(Calendar.MINUTE, minute);
-                alarm.set(Calendar.YEAR, 2015);
-                alarmActiviated = false;
-            }
-        }, mHour, mMinute, false);
-		
-		Button b2 = (Button)findViewById(R.id.b2);
-		b2.setOnClickListener(new View.OnClickListener() {
+		setContentView(R.layout.activity_main);
+		setUp();
+		Button b1 = (Button)findViewById(R.id.button1);
+		b1.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				tm.show ();
+				am.set( AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 
+						DISPLAY_DELAY, pi );
+				//AlarmClockInfo info = new AlarmClockInfo();
 			}
 		});
 	}
-	public void doWork() {
-	    runOnUiThread(new Runnable() {
-	        public void run() {
-	            try{
-	                Calendar c = Calendar.getInstance();
-	                
-	                if (c.compareTo(alarm) >= 0 && alarmActiviated == false){
-	                	Intent intent = new Intent(MainActivity.this,TimesUp.class);
-	                	startActivity(intent);
-	                	alarmActiviated = true;
-	                }
-	                
-	                //tv1.setText(hour+" "+minute+" "+second);
-	            }catch (Exception e) {}
-	        }
-	    });
+	public void setUp()
+	{
+		br = new BroadcastReceiver(){
+			@Override
+			public void onReceive(Context c, Intent i){
+				Toast.makeText(c, DISPLAY_DELAY/1000+" secs later", Toast.LENGTH_SHORT).show();
+			}
+		};
+		registerReceiver(br, new IntentFilter("me.timmui.mathalarm"));
+		pi = PendingIntent.getBroadcast(this,0,new Intent("me.timmui.mathalarm"),0);
+		am = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate t he menu; this adds items to the action bar if it is present.
+		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
@@ -96,18 +70,5 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	class CountDownRunner implements Runnable{
-	    // @Override
-	    public void run() {
-	            while(!Thread.currentThread().isInterrupted()){
-	                try {
-	                doWork();
-	                    Thread.sleep(1000); // Pause of 1 Second
-	                } catch (InterruptedException e) {
-	                        Thread.currentThread().interrupt();
-	                }catch(Exception e){
-	                }
-	            }
-	    }
-	}
+
 }
